ceb2581d6ea81c993a1664bc33383e95291d660b
shuai <zs673@york.ac.uk>
2016-11-19 18:24:14 +0000

    xxx

--- src/test/ResultReader.java
+++ src/test/ResultReader.java
@@ -29,6 +29,7 @@ public class ResultReader {
 				} catch (IOException e) {
 					System.out.println("no such file: " + filepath);
 				}
+				if(lines != null)
 				result += bigSet + "" + smallSet + "" + lines.get(0) + "\n";
 			}
 
@@ -48,6 +49,7 @@ public class ResultReader {
 					lines = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
 				} catch (IOException e) {
 				}
+				if(lines != null)
 				result += bigSet + "" + smallSet + "" + lines.get(0) + "\n";
 			}
 
@@ -67,6 +69,8 @@ public class ResultReader {
 					lines = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
 				} catch (IOException e) {
 				}
+				
+				if(lines != null)
 				result += bigSet + "" + smallSet + "" + lines.get(0) + "\n";
 			}
 
