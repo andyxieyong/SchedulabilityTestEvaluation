aaece5ebadd48dbe48e1cc7b70351da50e66bd28
zhaos <zhaos@DESKTOP-AKJHLMR>
2018-02-07 22:06:35 +0000

    xx

--- confidenceTest.sh
+++ confidenceTest.sh
@@ -4,12 +4,12 @@ pathServer = "/usr/userfs/z/zs673/"
 
 
 
-cd $pathServer+"SchedulabilityTestEvaluation/result"
+cd $pathServer"SchedulabilityTestEvaluation/result"
 pwd
 
 rm -rf *.txt
-cd $pathServer+"SchedulabilityTestEvaluation"
+cd $pathServer"SchedulabilityTestEvaluation"
 pwd
 
 
-LD_LIBRARY_PATH=src nohup java -cp $pathServer+"SchedulabilityTestEvaluation/bin" test.IdenticalTest &
+LD_LIBRARY_PATH=src nohup java -cp $pathServer"SchedulabilityTestEvaluation/bin" test.ConfidenceTest1000 &
--- identicalTest.sh
+++ identicalTest.sh
@@ -1,15 +1,4 @@
+pathMy="/home/shuai/Desktop/SchedulabilityTestEvaluation/src"
+pathServer=""
 
-pathMy="/home/shuai/Desktop/"
-pathServer="/usr/userfs/z/zs673/"
-
-
-
-cd $pathMy"SchedulabilityTestEvaluation/result"
-pwd
-
-rm -rf *.txt
-cd $pathMy"SchedulabilityTestEvaluation"
-pwd
-
-
-LD_LIBRARY_PATH=src nohup java -cp $pathMy"SchedulabilityTestEvaluation/bin" test.IdenticalTest &
+java -cp /home/shuai/Desktop/SchedulabilityTestEvaluation/bin -Djava.library.path="/home/shuai/Desktop/SchedulabilityTestEvaluation/src" test.IdenticalTest
