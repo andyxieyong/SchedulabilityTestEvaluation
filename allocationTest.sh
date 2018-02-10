c1117dc4cfd3659c48922a64cb259df268c219f0
Z Shuai <zs673@research2.york.ac.uk>
2018-02-10 18:33:32 +0000

    master

--- allocationTest.sh
+++ allocationTest.sh
@@ -4,7 +4,6 @@ pwd
 rm -rf *.txt
 cd /home/userfs/z/zs673/SchedulabilityTestEvaluation
 pwd
-
+rm nohup.out
 
 LD_LIBRARY_PATH=src nohup java -cp /home/userfs/z/zs673/SchedulabilityTestEvaluation/bin evaluationSection4.ResourceOrientedAllocationTest &
-zs673@csresearch1:~/SchedulabilityTestEvaluation$ 
