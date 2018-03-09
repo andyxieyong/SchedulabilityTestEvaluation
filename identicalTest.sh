7b958b82994f32e58f252c083f06a582d78bb826
zhaos <zhaos@DESKTOP-AKJHLMR>
2018-03-09 02:01:09 +0000

    x

--- identicalTest.sh
+++ identicalTest.sh
@@ -2,3 +2,6 @@ pathMy="/home/shuai/Desktop/SchedulabilityTestEvaluation/src"
 pathServer=""
 
 java -cp /home/shuai/Desktop/SchedulabilityTestEvaluation/bin -Djava.library.path="/home/shuai/Desktop/SchedulabilityTestEvaluation/src" test.IdenticalTest
+
+
+
